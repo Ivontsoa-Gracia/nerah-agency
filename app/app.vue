2d84435118f2046f89585bbef383b030d290b457
madarom <graciaandriamihamina@gmail.com>
2025-10-11 14:17:20 +0300

    Add woff2 font files and update font-face sources
    Added Pirulen-Regular.woff2 and HelveticaNeueRoman.woff2 to the public/font directory. Updated app.vue to reference the new woff2 files in @font-face declarations for improved font loading performance.

--- app/app.vue
+++ app/app.vue
@@ -4,42 +4,39 @@
   </NuxtLayout>
 </template>
 
-
 <script setup lang="ts">
 useHead({
   link: [
     {
-      rel: 'stylesheet',
-      href: 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'
+      rel: "stylesheet",
+      href: "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css",
     },
     {
-      rel: 'stylesheet',
-      href: 'https://cdn.boxicons.com/fonts/brands/boxicons-brands.min.css'
+      rel: "stylesheet",
+      href: "https://cdn.boxicons.com/fonts/brands/boxicons-brands.min.css",
     },
     {
-      rel: 'stylesheet',
-      href: 'https://cdn.boxicons.com/fonts/basic/boxicons.min.css'
+      rel: "stylesheet",
+      href: "https://cdn.boxicons.com/fonts/basic/boxicons.min.css",
     },
     {
-      rel: 'stylesheet',
-      href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+      rel: "stylesheet",
+      href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
     },
-  ]
-})
+  ],
+});
 </script>
 <style>
 @font-face {
-  font-family: 'Pirulen';
-  src: url('/fonts/Pirulen-Regular.otf') format('opentype');
-  font-weight: normal;
-  font-style: normal;
+  font-family: "Pirulen";
+  src: url("/font/Pirulen-Regular.woff2") format("woff2"),
+    url("/font/Pirulen-Regular.otf") format("opentype");
 }
 
 @font-face {
-    font-family: 'Helvetica';
-    src: url('/font/HelveticaNeueRoman.otf') format('opentype');
-    font-weight: normal;
-    font-style: normal;
+  font-family: "Helvetica";
+  src: url("//font/HelveticaNeueRoman.woff2") format("woff2"),
+    url("//font/HelveticaNeueRoman.otf") format("opentype");
 }
 ::-webkit-scrollbar {
   width: 6px;
